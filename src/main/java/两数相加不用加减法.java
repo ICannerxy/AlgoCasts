718019e5fbdee0c920d4a5953a26c3eeaf542788
xuyang <icanner@qq.com>
2020-04-19 22:26:33 +0800

    两数之和，不用算术运算符

--- src/main/java/两数相加不用加减法.java
+++ src/main/java/两数相加不用加减法.java
@@ -1,6 +1,27 @@
 /**
  * @author icanner
  * @date 2020/4/1811:07 下午
+ * @descrption 求两个数之和，不能用算术运算符
  */
 public class 两数相加不用加减法 {
+
+    public static void main(String[] args) {
+        两数相加不用加减法 item = new 两数相加不用加减法();
+        System.out.println(item.getSumRecursive(9, 11));
+    }
+
+    public int getSumRecursive(int a, int b) {
+        return b == 0 ? a : getSumRecursive(a ^ b, (a & b) << 1);
+    }
+
+    // Time O(m) Space: O(1)
+    public int getSumIterative(int a, int b) {
+        while (b != 0) {
+            int sum = a ^ b;
+            int carry = (a & b) << 1;
+            a = sum;
+            b = carry;
+        }
+        return a;
+    }
 }
