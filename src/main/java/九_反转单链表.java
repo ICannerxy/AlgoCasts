f81f4526bbb4fca32b4afefb8458a3b1f2e2ea1b
xuyang <icanner@qq.com>
2020-04-27 23:20:41 +0800

    九_反转单链表

---
+++ src/main/java/九_反转单链表.java
@@ -0,0 +1,23 @@
+/**
+ * @author icanner
+ * @date 2020/4/279:57 下午
+ */
+public class 九_反转单链表 {
+
+    private class ListNode {
+        int val;
+        ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head, pre = null;
+        while (null != cur) {
+             ListNode next = cur.next;
+
+        }
+    }
+}
