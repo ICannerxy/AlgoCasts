5b4596cd6bf35fc57ddaa37c7aeb409649aaf009
xuyang <icanner@qq.com>
2020-04-28 23:35:44 +0800

    九_反转单链表

--- src/main/java/九_反转单链表.java
+++ src/main/java/九_反转单链表.java
@@ -1,6 +1,7 @@
 /**
  * @author icanner
  * @date 2020/4/279:57 下午
+ * @descrption 反转单链表
  */
 public class 九_反转单链表 {
 
@@ -13,11 +14,22 @@ public class 九_反转单链表 {
         }
     }
 
+    /**
+     * T: O(n) S:O(1)
+     * 定义cur节点和节点，分别初始化
+     * 当cur节点不为null时，先把cur.next节点记录下来
+     *
+     * @param head
+     * @return
+     */
     public ListNode reverseList(ListNode head) {
         ListNode cur = head, pre = null;
         while (null != cur) {
-             ListNode next = cur.next;
-
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
         }
+        return pre;
     }
 }
